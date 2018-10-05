b9a1a1164ae5ac841efad9605e933644683f4937
Kai Blin <kblin@biosustain.dtu.dk>
2018-10-05 17:02:53 +0200

    job: Adjust to the slightly changed job info API
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- app/job/job.js
+++ app/job/job.js
@@ -33,7 +33,7 @@ angular.module('antismash.ui.bacterial.as_job', [])
                 .then(function (resp) {
                     job_info.filename = resp.data.filename;
                     job_info.status = resp.data.status;
-                    job_info.short_status = resp.data.short_status;
+                    job_info.state = resp.data.state;
                     job_info.result_url = resp.data.result_url;
                     job_info.submitted = Date.parse(resp.data.added_ts);
                     job_info.last_changed = Date.parse(resp.data.last_changed_ts);
